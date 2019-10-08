f2a977c535e2fdf4783285d71684aa65c9fac856
Divyansh Jha <divyanshj.16@gmail.com>
2019-10-08 22:09:47 +0530

    Add reproduce accuracy notebook

--- setup.sh
+++ setup.sh
@@ -3,5 +3,7 @@ source activate torch
 wget https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz
 tar -xf cifar-100-python.tar.gz
 mv cifar-100-python cifar100
+mkdir cifar100/models
+cp FINAL_WRN_NO_FL.pth cifar100/models
 python prepare_data.py
 rm cifar-100-python.tar.gz
